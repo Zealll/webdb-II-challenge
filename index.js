5925aeb16606041da9f49dc2e492cece1501f10f
Zealll <eriznis@gmail.com>
2019-04-01 20:36:35 -0400

    Added all the necessay code for getting started

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+      filename: './data/lambda.sqlite3'
+  },
+  debug: true
+}
+
+const db = knex(knexConfig)
 
 // endpoints here
 
@@ -12,3 +25,6 @@ const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+
+
---
+++ zoo/zoo-router.js
@@ -0,0 +1,19 @@
+// const router = require('express').Router();
+// const knex = require('knex');
+
+
+// const knexConfig = {
+//     client: 'sqlite3',
+//     useNullAsDefault: true,
+//     connection: {
+//         filename: './data/lambda.sqlite3'
+//     },
+//     debug: true
+// }
+
+// const db = knex(knexConfig)
+
+
+
+
+// module.exports = router;
