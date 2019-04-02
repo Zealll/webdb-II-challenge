088707c9dfd74a84028076a75168f654ede4bed5
Zealll <eriznis@gmail.com>
2019-04-01 20:48:10 -0400

    added GET

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ const db = knex(knexConfig)
 
 // endpoints here
 
+server.get('/api/zoo', (req, res) => {
+  db('zoos')
+  .then(animal => res.status(200).json(animal))
+  .catch(error => res.status(500).json(error))
+})
+
+
+
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
